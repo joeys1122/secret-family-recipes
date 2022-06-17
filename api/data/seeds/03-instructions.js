27b89161a7b45bef99e701f570312bd1ef2f2520
joeys1122 <josephs1122@gmail.com>
2022-06-17 15:41:11 -0400

    changed seed files

--- api/data/seeds/03-instructions.js
+++ api/data/seeds/03-instructions.js
@@ -10,6 +10,16 @@ const instructions = [
   { instruction_step:1, instruction_name:'Cut roast in half', recipe_id:3 },
   { instruction_step:2, instruction_name:'Pour soup mix over roast', recipe_id:3 },
   { instruction_step:3, instruction_name:'Cover and cook on low for 8 to 9 hours', recipe_id:3 },
+
+  { instruction_step:1, instruction_name:'Cover chicken in breadcrumbs', recipe_id:4 },
+  { instruction_step:2, instruction_name:'Place in fryer for 5 to 10 minutes', recipe_id:4 },
+
+  { instruction_step:1, instruction_name:'Cut roast in half', recipe_id:5 },
+  { instruction_step:2, instruction_name:'Pour soup mix over roast', recipe_id:5 },
+  { instruction_step:3, instruction_name:'Cover and cook on low for 8 to 9 hours', recipe_id:5 },
+
+  { instruction_step:1, instruction_name:'Pour blueberry mixture into pan', recipe_id:6 },
+  { instruction_step:2, instruction_name:'Bake in oven for 30 to 40 minutes', recipe_id:6 },
 ]
 
 exports.seed = function(knex) {
